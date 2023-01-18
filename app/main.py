f6d06010baab7cd3f6d22f5dbebb9defc5ce780d
dev1week <rondo2860@gmail.com>
2023-01-18 23:45:55 +0900

    영화 추천 api 기능 추가

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from typing import List, Optional
 from fastapi import FastAPI, Query 
 from resolver import random_items, random_genres_items
 from fastapi.middleware.cors import CORSMiddleware 
+from recommender import item_based_recommendation
 
 app = FastAPI()
 
@@ -25,4 +26,5 @@ async def user_based(params : Optional[List[str]]= Query(None)):
 
 @app.get("/item-based/{item_id}")
 async def item_based(item_id : str):
-    return {"message":"item vased : {item_id}"}
+    result = item_based_recommendation(item_id)
+    return {"message":"item vased : {result}"}
